9dd10b56b64fcdbe9384f052e760b5cddd7f3ee3
Kevin Altman <kevin@itsthatguy.com>
2017-08-29 12:58:17 -0400

    fixes allNames bug

--- src/lib/imageFiles.js
+++ src/lib/imageFiles.js
@@ -3,13 +3,13 @@ import path from 'path';
 
 const DEFAULT_IMAGE_DIR = path.join(__dirname, '..', 'img');
 
-export const dirFor = (type, imageDir) => {
+export const dirFor = (type, imageDir = DEFAULT_IMAGE_DIR) => {
   if (type) return path.join(imageDir, type);
   else return imageDir;
 };
 
-export const allNames = (type) => {
-  return fs.readdirSync(dirFor(type)).filter(function(imageFileName) {
+export const allNames = (type, imageDir = DEFAULT_IMAGE_DIR) => {
+  return fs.readdirSync(dirFor(type, imageDir)).filter(function(imageFileName) {
     return imageFileName.match(/\.png/);
   }).map(function(imageFileName) {
     return imageFileName.replace(/\.png/, '');
@@ -19,11 +19,11 @@ export const allNames = (type) => {
 export const allPaths = (type, imageDir = DEFAULT_IMAGE_DIR) => {
   var dir;
   dir = dirFor(type, imageDir);
-  return allNames(type).map(function(name) {
+  return allNames(type, imageDir).map(function(name) {
     return path.join(dir, name + '.png');
   });
 };
 
-export const pathFor = (type, fileName) => {
-  return path.join(dirFor(type), fileName + '.png');
+export const pathFor = (type, fileName, imageDir = DEFAULT_IMAGE_DIR) => {
+  return path.join(dirFor(type, imageDir), fileName + '.png');
 };
